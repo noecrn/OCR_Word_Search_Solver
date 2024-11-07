2b65322a71ee5edab8e2a0743589d801cf522668
noe.cornu <noe.cornu@epita.fr>
2024-11-07 16:54:37 +0100

    [UPDATE] is grid present on the image

--- source/_detection/src/main.c
+++ source/_detection/src/main.c
@@ -1,17 +1,11 @@
+#include "../include/grid_detection.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <err.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <err.h>
-#include <SDL2/SDL.h>
 
-SDL_Surface *load_image (const char *filename) {
-  SDL_Surface *image = SDL_LoadBMP(filename);
-  if (!image) {
-    errx(1, "Unable to load bitmap: %s", SDL_GetError());
-  }
-  return image;
-}
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   if (argc != 2) {
     printf("Usage: %s <filename>\n", argv[0]);
     exit(1);
@@ -22,12 +16,13 @@ int main (int argc, char *argv[]) {
     errx(EXIT_FAILURE, "Failed to initialize SDL: %s", SDL_GetError());
   }
 
-  SDL_Surface *image = load_image(filename);
+  SDL_Surface *image = IMG_Load(filename);
   if (!image) {
     errx(EXIT_FAILURE, "Failed to load image: %s", SDL_GetError());
   }
 
-  // Add your code to process the image here
+  int count = detect_grid(image);
+  printf("%d\n", count);
 
   SDL_FreeSurface(image);
   SDL_Quit();
